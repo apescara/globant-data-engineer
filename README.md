e0d60eb0bc4f706cb5da420ae9f2351d7e640dd2
andrespescara <andrespescara@gmail.com>
2024-10-07 23:42:53 -0300

    make data upload to the BBDD

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # Globant Data Engineer
 For the Data Engineer test for Globant, divided in the following sections:
 
-## 1.- 
-Will use Flask for the local development
+## 1.- API
+Will use Flask for the local development and the data will be uploaded directly to BigQuery. To simplify the solution will asume that the Google default credentials are properly set up in the evironment.
+
+The API accepts .csv files and uploads them directly to BigQuery, asumig the filename (without the extension) as table_id. 
+
+It has the following body:
+
+| Key | Type | Description | Values | Optional | 
+|-----|------|-------------|--------|----------|
+| file | File | File to upload | .csv files | NO |
+| table_id | Text | Name of the destiantion table | any str | YES |
+| column_names | Text | Column name, only replaces them when creating from scratch or when truncating | , separeted strs | YES |
+| write_disposition | Text | Write disposition, on truncation if column_names is not defined, sets the column name as 0 based index values | WRITE_TRUNCATE, WRITE_APPEND | YES |
+
+
+Example of POST call:
+
+``` bash
+curl --location 'http://127.0.0.1:5000' \
+--form 'file=@"/C:/Users/andre/OneDrive/Desktop/globant/jobs.csv"' \
+--form 'table_id="jobs"' \
+--form 'column_names="id,jobs"' \
+--form 'write_disposition="WRITE_TRUNCATE"'
+```
 
 ## 2.- SQL
 BigQuery will be used as Database for the test, in a proyect and dataset created for this. Given this decision, all the tables in the solutions will start with ```globant-apescara.data.*```.
--- api/app.py
+++ api/app.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from werkzeug.utils import secure_filename
 import pandas as pd
+from google.cloud import bigquery
 
-UPLOAD_FOLDER = os.path.join('files', 'uploads')
+UPLOAD_FOLDER = os.path.join("files", "uploads")
 ALLOWED_EXTENSIONS = {"csv"}
+PROJECT_ID = "globant-apescara"
+DATASET_ID = "data"
 
 app = Flask(__name__)
 app.secret_key = "session secret key"
@@ -12,21 +15,49 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 def allowed_file(filename) -> bool:
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.split(".")[-1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/", methods=["GET", "POST"])
-def uploadFile():
+def uploadFile() -> Response:
     if request.method == "POST":
-        # upload file flask
-        f = request.files.get("file")
-        data_filename = secure_filename(f.filename)
+        # get file in flask
+        file = request.files.get("file")
+        data_filename = secure_filename(file.filename)
 
         # Read data in pandas
-        data = pd.read_csv(os.path.join(app.config["UPLOAD_FOLDER"], data_filename))
+        try:
+            columns = request.form.get("column_names").split(",")
+        except:
+            columns = None
+        data = pd.read_csv(file, header=None, names=columns)
+        data.columns = data.columns.astype(
+            str
+        )  # pyarrow wont work with numbers as column names
 
-        return jsonify(data.to_json())
+        # Upload to BigQuery
+        client = bigquery.Client()
+
+        # Set table name
+        table_id = request.form.get("table_id")
+        table_id = data_filename.split(".")[0] if not table_id else table_id
+
+        # Set data write disposition
+        job_config = bigquery.LoadJobConfig()
+        write_disposition = request.form.get("write_disposition")
+        job_config.write_disposition = (
+            "WRITE_APPEND" if not write_disposition else write_disposition
+        )
+
+        client.load_table_from_dataframe(
+            dataframe=data,
+            destination=PROJECT_ID + "." + DATASET_ID + "." + table_id,
+            job_config=job_config,
+        )
+
+        return jsonify({"message": f"data successfully uploaded to {table_id}"})
     return jsonify({"message": "invalid request"})
+
 
 if __name__ == "__main__":
     app.run()
